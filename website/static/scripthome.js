f2b558c140b58549076e6077209108cc2d7c5b9c
juhum <anddd144@gmail.com>
2023-07-25 15:29:44 +0200

    navbar menu adjustments

--- website/static/scripthome.js
+++ website/static/scripthome.js
@@ -1,16 +1,41 @@
+var dropdown = document.getElementById("myDropdown");
+var mainContainer = document.getElementById("main-container");
+var isRotated = false; // Variable to keep track of button rotation state
+
 function dropdownMenu() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  dropdown.classList.toggle("show");
+
+  // Toggle the rotation class on the button
+  var button = document.querySelector('.dropbtn');
+  isRotated = !isRotated;
+  button.classList.toggle("rotate", isRotated);
+
+  // Add margin-top to main-container element
+  if (dropdown.classList.contains('show')) {
+    mainContainer.style.marginTop = dropdown.offsetHeight + 'px';
+  } else {
+    mainContainer.style.marginTop = '';
+  }
 }
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
+
+    // Reset button rotation state when clicking outside the dropdown
+    var button = document.querySelector('.dropbtn');
+    if (isRotated) {
+      isRotated = false;
+      button.classList.remove("rotate");
+    }
+
+    // Remove margin-top from main-container element
+    mainContainer.style.marginTop = '';
   }
 }
